01607087c8169fea70290b6608728a286f96b1c6
melika0m <kharoubmelika@gmail.com>
2025-09-28 10:05:59 +0000

    Update main adding direction and mode vrs eli 6ra viha chi

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -327,3 +327,4 @@ async def ws_detect(websocket: WebSocket):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+###ane yeweyli 6ereyt chi 
